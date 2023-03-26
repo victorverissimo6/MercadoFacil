e7d864ced532bb596c298814c574798ff6346d8c
Victor <110508234+victorverissimo6@users.noreply.github.com>
2023-03-25 23:12:51 -0300

    Add files via upload

---
+++ mercadofacil/src/test/java/com/ufcg/psoft/mercadofacil/repository/VolatilLoteRepositoryTest.java
@@ -0,0 +1,117 @@
+package com.ufcg.psoft.mercadofacil.repository;
+
+import com.ufcg.psoft.mercadofacil.model.Lote;
+import com.ufcg.psoft.mercadofacil.model.Produto;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+class VolatilLoteRepositoryTest {
+
+
+   @Autowired
+   VolatilLoteRepository driver;
+
+
+   Lote lote;
+   Lote resultado;
+   Produto produto;
+
+
+   @BeforeEach
+   void setup() {
+       produto = Produto.builder()
+               .id(1L)
+               .nome("Produto Base")
+               .codigoBarra("123456789")
+               .fabricante("Fabricante Base")
+               .preco(125.36)
+               .build();
+       lote = Lote.builder()
+               .id(1L)
+               .numeroDeItens(100)
+               .produto(produto)
+               .build();
+   }
+
+
+   @AfterEach
+   void tearDown() {
+       produto = null;
+       driver.deleteAll();
+   }
+
+
+   @Test
+   @DisplayName("Adicionar o primeiro Lote no repositorio de dados")
+   void salvarPrimeiroLote() {
+       resultado = driver.save(lote);
+
+
+       assertEquals(driver.findAll().size(),1);
+       assertEquals(resultado.getId().longValue(), lote.getId().longValue());
+       assertEquals(resultado.getProduto(), produto);
+   }
+
+
+   @Test
+   @DisplayName("Adicionar o segundo Lote (ou posterior) no repositorio de dados")
+   void salvarSegundoLoteOuPosterior() {
+       Produto produtoExtra = Produto.builder()
+               .id(2L)
+               .nome("Produto Extra")
+               .codigoBarra("987654321")
+               .fabricante("Fabricante Extra")
+               .preco(125.36)
+               .build();
+       Lote loteExtra = Lote.builder()
+               .id(2L)
+               .numeroDeItens(100)
+               .produto(produtoExtra)
+               .build();
+       driver.save(lote);
+
+
+       resultado = driver.save(loteExtra);
+
+
+       assertEquals(driver.findAll().size(),2);
+       assertEquals(resultado.getId().longValue(), loteExtra.getId().longValue());
+       assertEquals(resultado.getProduto(), produtoExtra);
+
+
+   }
+   
+   
+   @Test
+   @DisplayName("Atualizar um Lote do repositório de dados")
+   void testarAtualizarLote() {
+	   driver.save(lote);
+	   Produto novoProduto = Produto.builder()
+			   .id(3L)
+			   .nome("Novo Produto")
+			   .codigoBarra("567832401")
+			   .fabricante("Novo Fabricante")
+			   .preco(160.78)
+			   .build();
+	   Lote loteAtualizado = Lote.builder()
+			   .id(3L)
+			   .numeroDeItens(130)
+			   .produto(novoProduto)
+			   .build();
+	   resultado = driver.update(loteAtualizado);
+	   assertEquals(driver.findAll().size(), 1);
+	   assertEquals(resultado.getId().longValue(), loteAtualizado.getId().longValue());
+	   assertEquals(resultado.getProduto(), novoProduto);
+	   
+	   
+   }
+
+   
+}
